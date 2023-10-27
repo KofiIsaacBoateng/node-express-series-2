c035941fe5a3b6188abdd56b0fda1b2eb86cfeda
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2023-10-27 20:07:53 +0000

    feat: 02-nodejs-server-with-logs created server

--- 02-nodejs-server-with-logs/server.js
+++ 02-nodejs-server-with-logs/server.js
@@ -1,15 +1,20 @@
-const { format } = require('date-fns')
-const { v4: uuid } = require("uuid")
 const EventEmitter = require('events')
 const { logEvents } = require("./log-events")
+const http = require('http')
 
+const PORT = process.env.PORT || 5000
 class Emitter extends EventEmitter{ }
 const newEmmitter = new Emitter()
-const logTypes = ['ERROR', 'INFO']
-const randomType = Math.floor(Math.random() * 2)
 
-newEmmitter.on('log', (logtype, logMessage, logfile ) => logEvents(logtype, logMessage, logfile))
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method)
+})
 
-setTimeout(() => {
-    newEmmitter.emit('log', logTypes[randomType], 'Log message goes here. Usually an Error or Info log!', 'sys.log')
-}, 50)
+server.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
+
+
+// newEmmitter.on('log', (logtype, logMessage, logfile ) => logEvents(logtype, logMessage, logfile))
+
+// setTimeout(() => {
+//     newEmmitter.emit('log', logTypes[randomType], 'Log message goes here. Usually an Error or Info log!', 'sys.log')
+// }, 50)
