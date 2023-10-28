4680a10e27d3f10c47b857a32064833ddb2ca9ca
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2023-10-28 18:40:13 +0000

    feat: 01-event-emitters first event

--- 01-event-emitters/server.js
+++ 01-event-emitters/server.js
@@ -1,1 +1,18 @@
-console.log("You run server again")
+const { format } = require('date-fns')
+const { v4: uuid } = require("uuid")
+const EventEmitter = require('events')
+
+class Emitter extends EventEmitter{ }
+const newEmmitter = new Emitter()
+
+newEmmitter.on('log', (data) => {
+    try {
+        console.log(data)
+    } catch (error) {
+        console.log(err)
+    }
+})
+
+setTimeout(() => {
+    newEmmitter.emit('log', "Data received by event 'log'!")
+}, 3000)
